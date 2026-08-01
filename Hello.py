709d8b02d5215a80aeb7d5f88b15a0dd2ea87ed2
Tanish Jamdar <tanishjamdar2004@gmail.com>
2026-08-01 21:48:44 +0530

    Greeting message changed

--- Hello.py
+++ Hello.py
@@ -1,2 +1,16 @@
-print("Hello")
+def greet():
+    messages = [
+        "Hello!",
+        "Good morning!",
+        "Good afternoon!",
+        "Good evening!",
+        "Welcome!"
+    ]
+
+    for message in messages:
+        print(message)
+
+
+if __name__ == "__main__":
+    greet()
 
